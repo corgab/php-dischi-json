13a6a5023535494e7a5509e375853732cb35813f
corgab <corbanigabriele@gmail.com>
2024-05-08 17:03:31 +0200

    ADD: grafica decente

--- index.php
+++ index.php
@@ -9,16 +9,16 @@
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
+</head>
 
 </head>
 <body>
-    <div id="app">
+    <div id="app" class="bg-primary-subtle">
         <div class="container">
             <div class="row">
-                <div v-for="disk in disks" :key="disk" class="text-center col-4" style="width: 18rem;">
+                <div v-for="disk in disks" :key="disk" class="text-center col-4 py-3">
                     <div>
-                    <img :src="disk.poster" class="card-img-top" alt="">
+                        <img :src="disk.poster" class="card-img-top" alt="">
                     </div>    
                     <div class="py-3">
                         <h5 class="card-title">{{ disk.title}}</h5>
